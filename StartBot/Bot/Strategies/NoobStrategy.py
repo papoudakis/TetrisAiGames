8f283eadf654bc9bd6129dbf59731f32d3022cdf
papoudakis <papoudak@auth.gr>
2016-04-03 04:49:51 +0300

    errors fixed

--- StartBot/Bot/Strategies/NoobStrategy.py
+++ StartBot/Bot/Strategies/NoobStrategy.py
@@ -9,24 +9,25 @@ class NoobStrategy(AbstractStrategy):
         AbstractStrategy.__init__(self, game)
 
     def choose(self):
-        start1 = time.time()
+        #~ start1 = time.time()
         self.initGameState = self._game.getInitGameState();
         legalFields = self.initGameState.getLegalActions()
         
         bestFields, bestMoves = self.FirstLevelStates(legalFields)
         index = self.SecondLevelStates(bestFields)
-        
+        #~ print len(bestFields)
+        #~ print len(bestMoves)
 
         #~ bestFields[index].printField()
         #~ print legalFields[best_moves].numOfHoles()
         #~ print legalFields[best_moves].computeBumbiness()
-        end1 = time.time()
-        print end1 - start1
+        #~ end1 = time.time()
+        #~ print end1 - start1
         return bestMoves[index]
     
     
     def evaluate(self, legalField):
-		return 10*legalField.numOfCompleteRows() - 2*legalField.maxHeigth() - 4*legalField.numOfHoles() - 0.5*legalField.computeBumbiness()
+        return 10*legalField.numOfCompleteRows() - 2*legalField.maxHeigth() - 4*legalField.numOfHoles() - 0.5*legalField.computeBumbiness()
 
     def FirstLevelStates(self,legalFields):
         scores  = []
@@ -47,7 +48,7 @@ class NoobStrategy(AbstractStrategy):
         finalFields = []
         finalMoves =[]
         
-        MAX_FIELDS = max(5, len(fields))
+        MAX_FIELDS = min(3, len(fields))
         for i in range(MAX_FIELDS):
             finalFields.append(fields[scores_index[i]])
             finalMoves.append(moves[scores_index[i]]) 
@@ -57,17 +58,19 @@ class NoobStrategy(AbstractStrategy):
     def SecondLevelStates(self, legalFields):
         best_score = -float('Inf')
         #~ best_Fieds
+        i = 0
+        index = 0 
         for field in legalFields:
-            i = 0
             score = []
             nextState = GameState(copy.deepcopy(field), 0, 0,self.initGameState.nextPiece, None, [3, -1])
             legalFields2 = nextState.getLegalActions()
             for f in legalFields2.values():
                 score.append(self.evaluate(f))
-            max_score = max(score)
+            if score:
+                max_score = max(score)
             if max_score > best_score:
                 index = i
                 best_score = max_score
             i = i + 1
-        return i
+        return index
             
