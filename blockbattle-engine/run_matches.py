9531b40e5fd62633bcafe7b624818db70a53102e
Konstantinos <kostas93@hotmail.com>
2016-04-13 22:30:09 +0300

    removed join

--- blockbattle-engine/run_matches.py
+++ blockbattle-engine/run_matches.py
@@ -5,13 +5,12 @@ from time import sleep
 
 from multiprocessing import Process
 
-def f(name):
-    call('java -cp bin com.theaigames.blockbattle.Blockbattle  "python ../../TetrisAiGames/StartBot/BotRun.py "  "python ../../TetrisAiGames/StartBot/BotRun.py a" 2>>err.txt', shell=True)
-
+def f(i):
+    call('java -cp bin com.theaigames.blockbattle.Blockbattle  "python ../../TetrisAiGames/StartBot/BotRun.py "  "python ../../TetrisAiGames/StartBot/BotRun.py a" 2>>err.txt 1>out.txt', shell=True)
+    print 'Finished game ' +str(i)
 if __name__ == '__main__':
     for i in range(10):
-        p = Process(target=f, args=('bob',))
+        p = Process(target=f, args=(i,))
         p.start()
-        p.join()
 
 
