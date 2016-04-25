3b398ddb310bc23793f87c5459aa30f4fd58aa5c
papoudakis <papoudak@auth.gr>
2016-04-25 12:31:04 +0300

    read weights from file

--- StartBot/Bot/Strategies/QLearningStrategy.py
+++ StartBot/Bot/Strategies/QLearningStrategy.py
@@ -9,16 +9,43 @@ from GameState import GameState
 from AbstractStrategy import AbstractStrategy
 import time
 import copy
-
+import re
+import random
 
 class QLearningStrategy(AbstractStrategy):
     
     def __init__(self, game):
-        AbstractStrategy.__init__(self, game)        
-        self.weights = [7, -2, -10, -2, -0.2, 8.0]
+        AbstractStrategy.__init__(self, game)      
+        #~ fl = open('../../../blockbattle-engine/err.txt', 'r')
+        #~ from subprocess import call
+        #~ print call('pwd')
+        fl = open('err.txt', 'r')
+        temp = fl.readlines()
+        
+        #~ w = w[1:]
+        #~ w = w[:-2]
+        #~ w = re.split(',',w)
+        #~ sys.stderr.write('len(w) = ' + str(len(w)) + ' \n')
+        #~ sys.stderr.write('len(w) = ' + str(w) + ' \n')
+        #~ print len(w)
+        #~ print w
+        if len(temp)<3:  
+            #~ self.weights = [7, -10, -2,-0.3,  8.0 ,-0.5, 4]
+            self.weights = [random.random() for i in range(6)]
+            sys.stderr.write('prwth fora \n')
+        else:
+            sys.stderr.write('ola cool fora \n')
+            w = temp[-6]
+            w = w[1:]
+            w = w[:-2]
+            w = re.split(',',w)
+            #~ print w
+            self.weights = map(float, w)
+            #~ self.weights = [[float(i)] for i in w]
+        #~ self.weights = [0, 0]
         #~ self.weights = [7.00051502346005, -1.99524836714981, -10.000494055828849, -1.9896080848981463, -0.1727675923987584, 8.001975999894963]
-        self.alpha = 0.00005
-        self.discount = 1
+        self.alpha = 0.0004
+        self.discount = 0.0
         
     def choose(self):
         self.initGameState = self._game.getInitGameState();
@@ -29,13 +56,19 @@ class QLearningStrategy(AbstractStrategy):
         
         best_val, moves = self.getValue(legalFields, self.initGameState.currentPiece, self.initGameState.Round)
         
-        
+        if moves == tuple(['drop']):
+            return moves
         tempField = copy.deepcopy(legalFields[moves])
         tSpin = tempField.computeTspin(self.initGameState.currentPiece, moves)
         complete_rows =  tempField.numOfCompleteRows()
+        heights = tempField.computeHeigths()
         reward = tempField.computeReward(complete_rows, tSpin)
-        reward = self.initGameState.combo*(reward>0)  + reward
-        #~ reward = reward - max(tempField.computeHeigths())/3
+        reward = self.initGameState.combo*(reward>0)  + reward  
+        reward = (3*reward + (complete_rows)**2 + 5*tSpin**2)*(complete_rows>0) - 20*(complete_rows==0) 
+         #~ - max(heights)/3.0
+        sys.stderr.write(str(reward) + '\n')
+
+        #~ - max(tempField.computeHeigths())/30
         self.update(legalFields, moves, self.initGameState.currentPiece, self.initGameState.Round, reward)
         #~ sys.stderr.write('Qplayer' + str(moves) + '\n')
         return moves
@@ -54,7 +87,8 @@ class QLearningStrategy(AbstractStrategy):
         reward = self.initGameState.combo*(reward>0)  + reward
         heights = legalField.computeHeigths()
         w_heights = [0.5, 0.75, 0.75, 1, 1, 1, 1, 0.75, 0.75, 0.5] 
-        agg_heights = sum([a*b for a,b in zip(heights,w_heights)])
+        #~ agg_heights = sum([a*b for a,b in zip(heights,w_heights)])
+        agg_heights = sum(heights)
         numOfTholes = legalField.checkForTholes()
         #~ print legalField.computeCostLine(heights)
         
@@ -68,14 +102,14 @@ class QLearningStrategy(AbstractStrategy):
         coastline = legalField.computeCoastLine(holes)
         bumbiness = legalField.computeBumbines(heights)
         semiCompleteRows = legalField.computeSemiCompleteRows(holes)
-        features.append(reward)
+        #~ features.append(reward)
         features.append(max(heights))
         features.append(numOfHoles)
         features.append(bumbiness)
-        features.append(agg_heights)
+        #~ features.append(tSpin)
         features.append(numOfTholes**2)
-        #~ features.append(coastline)
-        #~ features.append(semiCompleteRows**2*(semiCompleteRows>1))
+        features.append(coastline)
+        features.append(semiCompleteRows**2*(semiCompleteRows>1))
         return features
     
     def getQValue(self, legalFields, moves, piece,Round):
@@ -94,11 +128,11 @@ class QLearningStrategy(AbstractStrategy):
         for moves in legalFields.keys():
             possibleStateQValues[moves] = self.getQValue(legalFields, moves, piece,Round)
         if len(possibleStateQValues.keys())==0:
-            return 0, "drop"
+            return 0, tuple(['drop'])
         else:
             return max(possibleStateQValues.values()), max(possibleStateQValues, key=possibleStateQValues.get)
     
-    
+        
     def update(self, legalFields, moves, piece, Round, reward):
         
     #  Should update your weights based on transition
